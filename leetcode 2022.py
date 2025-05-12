d088f70dfcc104196ccd2cb6ba86717bf4c9b549
Mamta Jangra <mamtajangra204@gmail.com>
2025-05-12 21:10:20 +0530

    Implement one_to_two_d function to convert a 1D list into a 2D matrix with specified dimensions

--- leetcode 2022.py
+++ leetcode 2022.py
@@ -15,3 +15,31 @@ original = [1,2,3,4]
 m = 2
 n = 2
 print(one_to_2d(original,m,n))    
+
+
+                                                          # or 
+'''matrix length should m*n and create subarray temporary n=0
+loop in original and append the elements in subarray if tn<n.Moreover,if tn==nmeans no more space to append then 
+append subarray to N for fresh values we need tn=0 again at last return N '''
+
+def one_to_two_d(original,m,n):
+    if len(original)!=m*n:
+        return []
+    N = []
+    sub_arr = []
+    tn = 0
+    for i in original:
+        if tn < n:
+            sub_arr.append(i)
+            tn+= 1
+            if tn == n:
+                N.append(sub_arr)
+                sub_arr = []
+                tn = 0
+    return N
+    
+
+original = [1,2,3,4]
+m = 2
+n = 2
+print(one_to_two_d(original,m,n))      
