302b001210c3c2296b5c6b0638424fa4d440d6b3
Mamta Jangra <mamtajangra204@gmail.com>
2025-05-14 15:12:40 +0530

    Restore majority_elements function with example usage

--- leetcode169.py
+++ leetcode169.py
@@ -1,3 +1,11 @@
+'''Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+'''
 # to find element which are present in maximum times firstly sort the array and find mid index if 
 # mid index is greater than that is the element present in maximum number of times 
 def majority_elements(nums):
