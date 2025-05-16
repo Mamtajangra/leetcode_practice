0cde0f8294dfa7efdc5ca0c2b7f64ef10d337105
Mamta Jangra <mamtajangra204@gmail.com>
2025-05-16 12:30:36 +0530

    Add max_profit1 function to calculate profit from stock prices

--- leetcode122.py
+++ leetcode122.py
@@ -29,3 +29,16 @@ print(total_profit(prices))
 # Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
 # Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
 # Total profit is 4 + 3 = 7.
+
+
+
+def max_profit1(prices):
+    profit = 0
+    for i in range(1,len(prices)):
+        if prices[i]>prices[i-1]:
+            x = prices[i]-prices[i-1]
+            profit = profit+x
+    return profit        
+
+prices=[7,1,5,3,6,4]
+print(max_profit1(prices))
