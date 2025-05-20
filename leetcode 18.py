95486f96e65a6911e65f4d832b77786ed792e104
Mamta Jangra <mamtajangra204@gmail.com>
2025-05-20 18:13:44 +0530

    Refactor four_sum function to improve clarity and efficiency by changing the storage of results from a set to a list, and enhancing the handling of duplicate elements.

--- leetcode 18.py
+++ leetcode 18.py
@@ -1,11 +1,26 @@
+'''Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
 
+    0 <= a, b, c, d < n
+    a, b, c, and d are distinct.
+    nums[a] + nums[b] + nums[c] + nums[d] == target
+
+You may return the answer in any order.
+Example 1:
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+'''
 # using two pointer approach firstly sort the array initialize set to store unique values
 def four_sum(nums,target):
     nums.sort()
-    M = set()
+    M = []
     # here we will fix left right ,i ,j means agar hamko 4 number chahiye to ensure karna hai ki i ke baad 3 numbers ho so that we wrote len-3
     for i in range(len(nums)-3):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
         for j in range(i+1,len(nums)-2):
+            if j > i+1 and nums[j] == nums[j-1]:
+                continue
             # in two pointer  firstly fix left and right,left is first element of j loop and right is one less from len(num)
             left = j+1
             right = len(nums)-1
@@ -18,7 +33,7 @@ def four_sum(nums,target):
                 total = nums[i]+nums[j]+nums[left]+nums[right]
                 x = [nums[i],nums[j],nums[left],nums[right]]
                 if total == target:
-                    M.add(tuple(x))    ## tuple is immutable so use it instead of list 
+                    M.append(x)    ## tuple is immutable so use it instead of list 
                     while left<right and nums[left]==nums[left+1]:
                         left = left+1
                     while left<right and nums[right]==nums[right-1]:   
@@ -34,7 +49,7 @@ def four_sum(nums,target):
 
                     
 # return list 
-    return list(M)                
+    return (M)                
 
 
 # check 
