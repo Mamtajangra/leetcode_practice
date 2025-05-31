398f046213f0cd080a00e43f1dfaaa91de967ac6
Mamta Jangra <mamtajangra204@gmail.com>
2025-05-31 22:24:59 +0530

    Fix formatting for better readability in one_to_2d and one_to_two_d functions

--- leetcode2022.py
+++ leetcode2022.py
@@ -7,14 +7,14 @@ The second group of n=2 elements in original, [3,4], becomes the second row in t
 '''
 
 def one_to_2d(original,m,n):
-    if len(original)!= m*n:   ## check length is equal to m*n
+    if len(original)!= m * n:   ## check length is equal to m*n
         return []           
         '''first of all we will initialize an empty list then a loop over m 
         because m is the length of original n denotes the columns,moreover we will find out rows like
         via slicing i*n to (i+1)*n and append this to row then return '''
     N = []
     for i in range(m):
-        row = original[i*n:(i+1)*n]
+        row = original[i* n:(i+1)* n]
         N.append(row)
     return N
 
@@ -32,7 +32,7 @@ loop in original and append the elements in subarray if tn<n.Moreover,if tn==n m
 append subarray to N for fresh values we need tn=0 again at last return N '''
 
 def one_to_two_d(original,m,n):
-    if len(original)!=m*n:
+    if len(original) != m * n:
         return []
     N = []
     sub_arr = []
