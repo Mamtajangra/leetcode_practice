2ba677fdae2c81010dbec0c089a70251c8a94b7f
Mamta Jangra <mamtajangra204@gmail.com>
2025-06-02 09:30:45 +0530

    Add implementation to find if a pair with a given absolute difference exists in an unsorted array

---
+++ pair.py
@@ -0,0 +1,22 @@
+'''Given an unsorted array and an integer x, the task is to find if there exists a pair of elements in the array whose absolute difference is x. 
+
+Examples: 
+Input: arr[] = [5, 20, 3, 2, 50, 80], x = 78
+    Output: Yes
+    Explanation: The pair is {2, 80}.
+
+    Input: arr[] = [90, 70, 20, 80, 50], x = 45
+    Output: No
+    Explanation: No such pair exists.'''
+
+
+def pair(arr,target):
+  for i in range(len(arr)):
+    for j in range(i+1,len(arr)):
+      if arr[j] - arr[i] == target:
+        return True
+  return False
+
+arr = [90, 70, 20, 80, 50]
+target = 45
+print(pair(arr,target))  
