5c411b7929a1f88d964628660ee433141c6661be
Mamta Jangra <mamtajangra204@gmail.com>
2025-06-29 09:57:02 +0530

    Implement function to calculate maximum number of different types of candies Alice can eat

---
+++ leetcode575.py
@@ -0,0 +1,26 @@
+'''Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she visited a doctor.
+The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
+Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
+Example 1:
+Input: candyType = [1,1,2,2,3,3]
+Output: 3
+Explanation: Alice can only eat 6 / 2 = 3 candies. Since there are only 3 types, she can eat one of each type.
+Example 2:
+Input: candyType = [1,1,2,3]
+Output: 2
+Explanation: Alice can only eat 4 / 2 = 2 candies. Whether she eats types [1,2], [1,3], or [2,3], she still can only eat 2 different types.
+'''
+
+## store list elements into set so duplicate remove and find length of set
+## find the half of the length of list
+## find minimum of set length and half length
+
+
+def candies(candyType):
+    n = len(candyType)
+    candyType = len(set(candyType))
+    unique = n//2
+    return min(unique,candyType)
+
+candyType = [1,1,2,3]
+print(candies(candyType))
