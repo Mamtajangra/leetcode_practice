ab0ab76decbb11c21b2f7b4a8c7875e36a611514
Mamta Jangra <mamtajangra204@gmail.com>
2025-07-08 18:37:27 +0530

    Refactor spacing for improved readability in most_water1 function

--- leetcode11.py
+++ leetcode11.py
@@ -42,16 +42,19 @@ print(most_water(height))
 def most_water1(height):
     maximum = 0
     left = 0
-    right = len(height)-1
+    right = len(height) - 1
+
     while left < right:
         w = right - left
-        h = min(height[left],height[right])
+        h = min(height[left] , height[right])
         area = w * h 
         maximum = max(maximum,area)
         if height[left] < height[right]:
              left += 1
         else:
              right -= 1
+
+             
     return maximum  
 
 
