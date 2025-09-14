45f88fd61eeb5f3845ca66921c3bd2d3fa3cb90e
Mamta Jangra <mamtajangra204@gmail.com>
2025-09-14 17:00:00 +0530

    updated

--- leetcode15.py
+++ leetcode15.py
@@ -37,28 +37,29 @@ print(sum_three(nums))
 
 def sum_three(nums):
     nums.sort()
-    M=set()
+    m = []
     for i in range(len(nums)-2):
-            left=i+1
-            right = len(nums)-1
-            while left< right:
-                total = nums[left]+nums[right]+nums[i]
-                if total ==0:
-                    x = [nums[i],nums[left],nums[right]]
-                    M.add(tuple(x))
-                    while left<right and nums[left] == nums[left+1]:
-                        left = left+1
-                    while left<right and  nums[right] == nums[right-1]:
-                        right = right-1  
-                    left+=1
-                    right-=1    
-                elif total<0:
-                    left = left+1
-                        
-            
+            if i >0 and nums[i]== nums[i-1]:
+                continue
+            left = i+1
+            right= len(nums)-1
+            while left < right:
+                res = [nums[i],nums[left],nums[right]]
+                x = nums[i]+nums[left]+nums[right]
+                if x == 0:
+                    m.append(res)
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right]== nums[right- 1]:
+
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif x <0:
+                    left += 1
                 else:
-                    right = right-1
-    return list(M) 
+                    right -= 1
+    return m 
 
 
 nums = [-1,0,1,2,-1,-4]
