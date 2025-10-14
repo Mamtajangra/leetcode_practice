d7080a0047083a2949ffa897fb9b9b32e9723495
Mamta Jangra <mamtajangra204@gmail.com>
2025-10-14 07:58:33 +0530

    Implement GCD calculation for sum of first n odd and even numbers

---
+++ leetcode3658.py
@@ -0,0 +1,22 @@
+'''You are given an integer n. Your task is to compute the GCD (greatest common divisor) of two values:
+sumOdd: the sum of the first n odd numbers.
+sumEven: the sum of the first n even numbers.
+Return the GCD of sumOdd and sumEven.
+Example 1:
+Input: n = 4
+Output: 4
+Explanation:
+    Sum of the first 4 odd numbers sumOdd = 1 + 3 + 5 + 7 = 16
+    Sum of the first 4 even numbers sumEven = 2 + 4 + 6 + 8 = 20
+Hence, GCD(sumOdd, sumEven) = GCD(16, 20) = 4'''
+import math
+def gcd(n):
+    sum_odd = n*n
+    sum_even = n*(n+1)
+    gcd_final = math.gcd(sum_odd,sum_even)
+    return gcd_final
+
+
+n = 4
+print(gcd(n))
+        
