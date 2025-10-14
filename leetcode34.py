bd98442e29a7d157c89e99949da2c434d53db285
Mamta Jangra <mamtajangra204@gmail.com>
2025-10-14 10:04:52 +0530

    Implement function to find starting and ending positions of a target in a sorted array

---
+++ leetcode34.py
@@ -0,0 +1,32 @@
+'''Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity.
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+Input: nums = [], target = 0
+Output: [-1,-1]
+ '''
+
+def position(nums,target):
+    m = []
+    for i in range(len(nums)):
+        if nums[i] == target:
+            m.append(i)
+            break
+    if not m:
+        return[-1,-1]        
+            # if unable to find first occurrence find last
+    for j in range(len(nums)-1,-1,-1):
+        if nums[j] == target:
+            m.append(j)
+            break
+    return m   
+
+nums = [5,7,7,8,8,10]
+target = 6
+print(position(nums,target)) 
