bbffe0dbb6f86ed40ae447562d12f1824f6a91e6
Mamta Jangra <mamtajangra204@gmail.com>
2025-10-28 12:18:34 +0530

    search  2D matrix

---
+++ leetcode74.py
@@ -0,0 +1,24 @@
+'''You are given an m x n integer matrix matrix with the following two properties:
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+You must write a solution in O(log(m * n)) time complexity.
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+'''
+def search_mat(matrix,target):
+    for row in matrix:
+
+        if target in row:
+            return True
+        
+    return False
+
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+target = 13
+print(search_mat(matrix,target))
+   
