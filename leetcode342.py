4d1626e021c9fd8d03c1022015ad5165d43e665b
Mamta Jangra <mamtajangra204@gmail.com>
2025-10-29 14:00:50 +0530

    to find power of four

---
+++ leetcode342.py
@@ -0,0 +1,27 @@
+'''Given an integer n, return true if it is a power of four. Otherwise, return false. An integer n is a power of four, if there exists an integer x such that n == 4x.
+Example 1:
+Input: n = 16
+Output: true
+Example 2:
+Input: n = 5
+Output: false
+Example 3:
+Input: n = 1
+Output: true
+'''
+
+def pow_four(n):
+    if n<=0:
+        return False
+    if n==1:
+        return True
+    while n % 4 == 0:
+        n = n//4
+    if n == 1:
+        return True
+    return False  
+
+
+n = 5
+print(pow_four(n))
+        
